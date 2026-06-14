d02656cb6523dce077ac10ffc2c44d79f4b1ee66
Luck16048 <karinaparhomenko2007@gmail.com>
2026-06-14 20:07:32 +0200

    feat: Add audit service integration

--- src/main/java/com/example/AnimalShelter/service/AnimalService.java
+++ src/main/java/com/example/AnimalShelter/service/AnimalService.java
@@ -3,9 +3,17 @@ package com.example.AnimalShelter.service;
 import com.example.AnimalShelter.entity.AnimalEntity;
 import com.example.AnimalShelter.repository.AnimalRepository;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class AnimalService {
@@ -16,25 +24,59 @@ public class AnimalService {
     }
 
     public List<AnimalEntity> getAll() {
-        return animalRepository.findAll();
+        List<AnimalEntity> animal = animalRepository.findAll();
+        sendToAudit(animal);
+        return animal;
     }
 
     public AnimalEntity getById(long id) {
-        return animalRepository.findById(id);
+        AnimalEntity animal = animalRepository.findById(id);
+        sendToAudit(animal);
+        return animal;
     }
 
     public AnimalEntity post(AnimalEntity animalEntity) {
-        return  animalRepository.save(animalEntity);
+        AnimalEntity animal = animalRepository.save(animalEntity);
+        sendToAudit(animal);
+        return  animal;
     }
 
     public AnimalEntity put(AnimalEntity animalEntity) {
-        return animalRepository.update(animalEntity);
+        AnimalEntity animal = animalRepository.update(animalEntity);
+        sendToAudit(animal);
+        return animal;
     }
 
     public AnimalEntity patch(AnimalEntity animalEntity) {
-        return animalRepository.update(animalEntity);
+        AnimalEntity animal = animalRepository.update(animalEntity);
+        sendToAudit(animal);
+        return animal;
     }
     public void delete(long id) {
         animalRepository.delete(id);
+        sendToAudit(id);
+    }
+
+    private final HttpClient httpClient = HttpClient.newHttpClient();
+
+    private void sendToAudit(Object request) {
+        try {
+            String json = new ObjectMapper().writeValueAsString(
+                    Map.of(
+                            "id", UUID.randomUUID().toString(),
+                            "request", new ObjectMapper().writeValueAsString(request),
+                            "createdAt", LocalDate.now().toString()
+                    )
+            );
+            HttpRequest httpRequest = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:8081/audit"))
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(json))
+                    .build();
+
+            httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
